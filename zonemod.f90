e888b936b4d5a9433477e28370ab123c216e8e03
Fei Yuan <yuan@nscl.msu.edu>
2014-06-05 18:27:24 -0400

    These seem to be good values for the Solar wind.
    Density at 1AU = 34e12 kg/AU^3 = 6 particles/cm^3
    Speed   at 1AU = 100 AU/year   = 470 km/s

--- zonemod.f90
+++ zonemod.f90
@@ -4,8 +4,8 @@ module zone
   integer :: nleftx,  nlefty,  nleftz       ! xyz lower boundary condition
   integer :: nrightx, nrighty, nrightz      ! xyz upper boundary condition
 
-  integer, parameter :: imax = 100
-  integer, parameter :: jmax = 200
+  integer, parameter :: imax = 60
+  integer, parameter :: jmax = 60
   integer, parameter :: kmax = 1
 
   real, dimension(imax, jmax, kmax) :: zro, zpr, zux, zuy, zuz, zfl
@@ -18,25 +18,25 @@ module zone
   ! Mass: 10^12 kg
   ! Time: year
 
-  real, parameter :: box_xmin = -10
-  real, parameter :: box_xmax = 10
+  real, parameter :: box_xmin = -4
+  real, parameter :: box_xmax = 4
 
-  real, parameter :: box_ymin = -20
-  real, parameter :: box_ymax = 20
+  real, parameter :: box_ymin = -4
+  real, parameter :: box_ymax = 4
 
   real, parameter :: ambient_density = 6
   real, parameter :: ambient_pressure = 32.8
 
   real, parameter :: injection_radius = .5
 
-  real, parameter :: wind_density = 94
-  real, parameter :: wind_pressure = 3e5
+  real, parameter :: wind_density = 180
+  real, parameter :: wind_pressure = 6e5
 !  real, parameter :: wind_speed = 1000.
 
-  real, parameter :: shock_density = 20
-  real, parameter :: shock_pressure = 1e5
+  real, parameter :: shock_density = 35
+!  real, parameter :: shock_pressure = 1e5
   real, parameter :: shock_velocity = 100
-  real, parameter :: shock_start_time = 2
+  real, parameter :: shock_start_time = 1.5
 
   real, parameter :: sun_origin_x = 0
   real, parameter :: sun_origin_y = 0
